7cfbc53ee6153ccdcc8979139ef8c57c2e21d1a1
David Eby <dqeby606@learner.dvusd.org>
2026-08-25 11:07:32 -0700

    Added functionality
    Test in CodeHS, new additions soon

---
+++ react-js/react.jsx
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, View, StyleSheet, TextInput } from 'react-native';
+import Constants from 'expo-constants';
+
+export default class App extends Component {
+    state = {
+        String, username: "",
+        String, org: "",
+        String, contact: "",
+        String, access: ""
+    };
+
+    _handleUsername = username => {
+        this.setState({ username });
+    }
+    _handleDepartment = org => {
+        this.setState({ org });
+    }
+    _handleContact = contact => {
+        this.setState({ contact });
+    }
+    _handleAccess = access => {
+        this.setState({ access });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Full Name: </Text>
+                <TextInput
+                    value={this.state.username}
+                    onChangeText={this._handleUsername}
+                    style={styles.input}
+                    maxLength={29}
+                />
+                
+                <Text>Department: </Text>
+                <TextInput
+                    value={this.state.org}
+                    onChangeText={this._handleDepartment}
+                    style={styles.input}
+                    maxLength={24}
+                />
+
+                <Text>Contact: </Text>
+                <TextInput
+                    value={this.state.contact}
+                    onChangeText={this._handleContact}
+                    style={styles.input}
+                    maxLength={27}
+                />
+
+                <Text>Access: </Text>
+                <TextInput
+                    value={this.state.access}
+                    onChangeText={this._handleAccess}
+                    style={styles.input}
+                    maxLength={27}
+                />
+                
+                <View style={styles.box}>
+                    <Text style={[styles.outputTxt, styles.header]}>
+                        Apex Entertainment Pass
+                    </Text>
+                    <View style={styles.line}/>
+                    <Text style={styles.outputTxt}>
+                        Attendee: {this.state.username}
+                    </Text>
+                    <Text style={styles.outputTxt}>
+                        Org: {this.state.org}
+                    </Text>
+                    <Text style={styles.outputTxt}>
+                        Contact: {this.state.contact}
+                    </Text>
+                    <Text style={styles.outputTxt}>
+                        Access: {this.state.access}
+                    </Text>
+                    <Text style={styles.outputTxt}>
+                        Issuer: BCH Software Terminal System
+                    </Text>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: Constants.statusBarHeight,
+        backgroundColor: '#ecf0f1',
+    },
+    input: {
+        width: 225,
+        height: 44,
+        padding: 8,
+        borderWidth: 2,
+        bordorColor: 'black'
+    },
+    box: {
+        justifyContent: 'center',
+        width: 275,
+        height: 225,
+        backgroundColor: "lightblue",
+        margin: 25,
+        borderCurve: 'continuous',
+        borderRadius: 20
+    },
+    outputTxt: {
+        margin: 5,
+        fontSize: 15,
+        fontFamily: 'Times New Roman',
+        textAlign: 'left'
+    },
+    header: {
+        fontSize: 20,
+        textAlign: 'center'
+    },
+    line: {
+        borderBottomColor: '#050505',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        marginVertical: 7,
+        width: '100%',
+    }
+});

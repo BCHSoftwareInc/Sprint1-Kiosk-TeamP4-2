c4eeaa3a68be2d7e58759b0f3c44d88186e0cb65
David Eby <dqeby606@learner.dvusd.org>
2026-08-27 11:05:38 -0700

    Changed something for more security
    Changed input label "contact" to "email"

--- react-js/react.jsx
+++ react-js/react.jsx
@@ -40,7 +40,7 @@ export default class App extends Component {
                     style={styles.input}
                 />
 
-                <Text>Contact: </Text>
+                <Text>Email: </Text>
                 <TextInput
                     value={this.state.contact}
                     onChangeText={this._handleContact}
